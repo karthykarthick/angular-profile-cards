9e6cc1c5e60033cb7e37185c24db48d5a3e080ca
karthick <karthick_h@zakapps.com>
2021-08-23 16:50:12 +0530

    added ngfor to display posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-profile-cards';
+  posts = [{
+    title: 'Neat Tree',
+    imageUrl: 'assets/tree.jpeg',
+    username: 'nature',
+    content: 'I saw this neat tree today'
+  },
+  {
+    title: 'Snowy Mountain',
+    imageUrl: 'assets/mountain.jpeg',
+    username: 'Mountain_lover',
+    content: 'Here is the picture of the mountain'
+  },
+  {
+    title: 'Favourite hobby is Biking',
+    imageUrl: 'assets/biking.jpeg',
+    username: 'pro_rider',
+    content: 'I love to explore the roads with my Bike'
+  }
+ ];
 }
